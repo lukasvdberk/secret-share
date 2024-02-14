241698475159ad8c836a19ba071b44f0351ed641
lukasberk <lukasvdberk@gmail.com>
2024-02-14 16:33:24 +0100

    set table definition

--- src/services/utils/dynamo-db.ts
+++ src/services/utils/dynamo-db.ts
@@ -1,19 +1,52 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import {DynamoDB, DynamoDBClient} from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocumentClient, GetCommand, PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb"
+import {secretTable} from "@/services/secrets-table";
 
 /**
  * Connect to DynamoDB and return the connection
  */
 export async function connectToDynamoDb() {
-    const dbClient = new DynamoDBClient({
+    const configuration = {
         endpoint: process.env.DYNAMODB_ENDPOINT as string,
         region: process.env.DYNAMODB_REGION as string,
         credentials: {
             accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
             secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
         },
-    })
+    }
+    const dbClient = new DynamoDBClient(configuration)
+    const ddb = new DynamoDB(configuration);
     const docClient = DynamoDBDocumentClient.from(dbClient)
+
+    // create table if it does not exist
+    try {
+        await ddb.createTable({
+            "AttributeDefinitions": [
+                {
+                    "AttributeName": "id",
+                    "AttributeType": "S"
+                },
+                {
+                    "AttributeName": "secretValue",
+                    "AttributeType": "S"
+                },
+                {
+                    "AttributeName": "hasBeenRead",
+                    "AttributeType": "B"
+                }
+            ],
+            "KeySchema": [
+                {
+                    "AttributeName": "id",
+                    "KeyType": "HASH"
+                }
+            ],
+            TableName: secretTable
+        })
+    }
+    catch (e) {
+        console.error('Creating table failed', e)
+    }
 
     return docClient
 }
